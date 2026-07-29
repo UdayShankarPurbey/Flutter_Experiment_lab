036aa9e9cf4c6713e757f1b53285d0355e62d7fd
Uday Shankar Purbey <uday.naxtre@gmail.com>
2026-07-29 13:08:51 +0530

    feat: add basic variable type demonstrations
    - Added integer and BigInt examples
    - Added double and num type examples
    - Added boolean variable demonstration
    - Commented out previous hello world logic

--- basics.dart
+++ basics.dart
@@ -1,16 +1,44 @@
 import 'dart:io';
 
 void main() {
-  print('hello world');
-  stdout.write('Enter your name : ');
-  var name = stdin.readLineSync();
-  stdout.write("Written Name is : $name");
+  // print('hello world');
+  // stdout.write('Enter your name : ');
+  // var name = stdin.readLineSync();
+  // stdout.write("Written Name is : $name");
 
 
-  var raman = new Human();
-  var ramana = Human();
+  // var raman = new Human();
+  // var ramana = Human();
 
-  print("$raman  ,$ramana");
+  // print("$raman  ,$ramana");
+
+  //declaration of a variable 
+  // int? a ; //here ? means this value may be null
+  int a ;
+  // assign value
+  a = 7; // initailiasation
+  print(a);
+
+//inline declaration
+  int b = 24;
+
+  print(b);
+
+  BigInt c = BigInt.parse(('12345878787555555555'));
+
+  print(c);
+
+  double percentage = 99.26;
+
+  print(percentage);
+
+  num val = 0.12;
+
+  print(val);
+
+  bool isLoggedIn = false;
+
+  print(isLoggedIn);
 }
 
 
